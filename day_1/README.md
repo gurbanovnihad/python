45d04dc4d77187420dcac045ed14c0e484ae59ef
Nihad Gurbanov <7404@holbertonstudents.com>
2023-11-23 10:38:09 -0800

    day_1

---
+++ day_1/README.md
@@ -0,0 +1,3 @@
+Band name generator
+
+day 1 covers variables, print and input functions and string formatting in Python.
---
+++ day_1/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#band name generator takes city and pet names and generates a band name.
+
+print("Welcome to the Band Name Generator\n")
+city = input("What's the name of the city you grew up in? ")
+pet = input("What's your pet's name? ")
+print("\nYour band name could be {} {}.".format(city, pet))
+
